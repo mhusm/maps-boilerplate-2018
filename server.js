e9db3a05f567778fd048919c1f07a24ac680dc66
Maria Husmann <maria.husmann@gmail.com>
2018-10-05 15:04:32 +0200

    changes in server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var path = require('path');
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let path = require('path');
+let serveStatic = require('serve-static');
 
 // serve the index.html as starting page
 app.get('/', function (req, res) {
@@ -9,7 +10,8 @@ app.get('/', function (req, res) {
 });
 
 // serve all files in dist
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
+app.use(serveStatic(__dirname + "/dist"));
 
 http.listen(process.env.PORT || 8090, function(){
     console.log(`listening on *: ${http.address().port}`);
