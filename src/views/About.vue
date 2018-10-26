033a0d1f75f88f59162bfbe41f6cc929b00b5118
Maria Husmann <maria.husmann@gmail.com>
2018-10-26 11:00:17 +0200

    adding content from contentful

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div >{{title}}</div>
+    <img :src="image"/>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import style from "@/styles.js";
+import SnazzyInfoWindow from "snazzy-info-window";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  data: function () {
+    return {
+      title: "",
+      image: "",
+    }
+  },
+  mounted: function() {
+    contentfulClient.getEntry("6BVJUl5RuMyU48W2MIuAsc").then(entry => {
+      this.title = entry.fields.title;
+      console.log(entry.fields.portrait.fields.file.url);
+      this.image = entry.fields.portrait.fields.file.url;
+    });
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,47 +10,44 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import style from "@/styles.js";
-import SnazzyInfoWindow from 'snazzy-info-window'
+import SnazzyInfoWindow from "snazzy-info-window";
 
 export default {
   name: "home",
   components: {
     HelloWorld
   },
-  mounted: function(){
-    const element = document.getElementById("map")
- 
+  mounted: function() {
+    const element = document.getElementById("map");
+
     const options = {
-        zoom: 14,
-        center: new google.maps.LatLng(47.071467, 8.277621),
-        styles: style
+      zoom: 14,
+      center: new google.maps.LatLng(47.071467, 8.277621),
+      styles: style
     };
 
     this.map = new google.maps.Map(element, options);
     const plcoords = [
-      {lat: 47.071978, lng: 8.262165 },
-      {lat: 47.072978, lng: 8.292165 },
-      {lat: 47.073978, lng: 8.292165 },
-      {lat: 47.074978, lng: 8.282165 }
+      { lat: 47.071978, lng: 8.262165 },
+      { lat: 47.072978, lng: 8.292165 },
+      { lat: 47.073978, lng: 8.292165 },
+      { lat: 47.074978, lng: 8.282165 }
     ];
-let polygon = new google.maps.Polygon({
-  path: plcoords,
-  strokeColor: '#4286f4',
-  strokeOpacity: 1.0,
-  strokeWeight: 2,
-  fillColor: '#FF0000',
-  fillOpacity: 0.35,
-  map: this.map
-});
+    let polygon = new google.maps.Polygon({
+      path: plcoords,
+      strokeColor: "#4286f4",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: this.map
+    });
   }
-
-
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
 #map {
   height: 80vh;
   width: 80vw;
